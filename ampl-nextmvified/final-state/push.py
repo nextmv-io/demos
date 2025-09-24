93d4e05e2a0ccd39b95abde3d6896a0a9b26d716
Nicole Misek <nicole@nextmv.io>
2025-09-24 11:27:04 -0400

    Updates ampl example

--- ampl-nextmvified/final-state/push.py
+++ ampl-nextmvified/final-state/push.py
@@ -2,23 +2,10 @@ import os
 
 from nextmv.cloud import Application, Client
 
-# import sys
-
-
 api_key = os.environ.get("NEXTMV_API_KEY")
 if api_key is None:
     raise Exception("Please set NEXTMV_API_KEY environment variable")
 
-# version_id = None
-# if len(sys.argv) > 1:
-#     version_id = sys.argv[1]
-# else:
-#     version_id = os.environ.get("VERSION_ID")
-
-# if version_id is None:
-#     raise Exception(
-#         "Please provide VERSION_ID either as command line argument or environment variable"
-#     )
 
 client = Client(api_key=api_key)
 if Application.exists(client, id="ampl-example"):
@@ -29,8 +16,3 @@ else:
 
 app_dir = os.path.dirname(os.path.abspath(__file__))
 app.push(app_dir=app_dir, verbose=True)
-# app.new_version(id=version_id, name=version_id)
-# if app.instance_exists("staging"):
-#     app.update_instance(version_id=version_id, id="staging", name="staging")
-# else:
-#     app.new_instance(version_id=version_id, id="staging", name="staging")
